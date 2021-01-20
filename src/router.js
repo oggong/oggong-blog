d67c492c809c5c01cef3afc4bfe98ded019a2e0c
oggong <49095304+oggong@users.noreply.github.com>
2021-01-20 21:13:47 +0900

    0120 push

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Router from "vue-router";
+import AppHeader from "./layout/AppHeader";
+import AppFooter from "./layout/AppFooter";
+import Components from "./views/Components.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  linkExactActiveClass: "active",
+  routes: [
+    {
+      path: "/",
+      name: "components",
+      components: {
+        header: AppHeader,
+        default: Components,
+        footer: AppFooter,
+      },
+    },
+  ],
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
+});
